bc66caac451a9c48afb6d89e4ab021368addbacf
tystar86 <stepanka.lucinova@gmail.com>
2017-07-10 08:42:03 +0200

    refactoring

--- license_check.py
+++ license_check.py
@@ -1,34 +1,40 @@
-import requests, json, requests_cache, base64, re, difflib, sys
+import requests, json, requests_cache, base64, re, difflib, sys, argparse
 from requests.auth import HTTPBasicAuth
 from datetime import datetime
 
-f = ''
 def main():
-    global f
-    requests_cache.install_cache('github_cache', backend='sqlite', expire_after=36000)
-    create_file()
-    token = get_token()
-    list_all_repos = all_repositories(token)
+    parser = argparse.ArgumentParser(description="Apiary License Check")
+    parser.add_argument("filename",
+                        help="Enter a file to compare.",
+                        nargs="?")
+    args = parser.parse_args()
+    f = create_file()
+    license_check(f)
+    if args.filename:
+        compare_files(f.name, args.filename)
 
+def license_check(f):
+    requests_cache.install_cache('github_cache', backend='sqlite', expire_after=3600000)
+    token = get_token()
+    list_all_repos = all_repositories(token, f)
     repos_without_license = []
     list_without_licence = []
-
     for every_repo in list_all_repos:
         print('REPO: ', every_repo, file=f)
-        contents_url = json_parsing('https://api.github.com/repos/apiaryio/' + every_repo + '/contents', token)
+        contents_url = json_parsing('https://api.github.com/repos/apiaryio/' + every_repo + '/contents', token, f)
         list_licenses = []
         find_license_file = False
         find_license = False
         for filename in contents_url:
             if re_findall(filename['name'], r'\blicen[sc]es?d?'):
-                r = search_license_license(filename, token)
+                r = search_license_type_in_license_file(filename, token, f)
                 list_licenses.append((filename['name'], r))
                 print(filename['name'], ': ', r, file=f)
                 if r:
                     find_license = True
 
             if re_findall(filename['name'], r'\breadme') or filename['name'] == 'package.json':
-                v = search_license_readme_packagejson(filename, token)
+                v = search_license_type_in_readme_packagejson(filename, token, f)
                 list_licenses.append((filename['name'], v))
                 print(filename['name'], ': ', v, file=f)
                 if v:
@@ -44,63 +50,38 @@ def main():
         if not find_license:
             list_without_licence.append(every_repo)
 
-
-        pull_url = json_parsing('https://api.github.com/repos/apiaryio/' + every_repo + '/pulls', token)
-        if pull_url != []:
-            for g in pull_url:
-                pull_licenses = head_sha(every_repo, g['head']['sha'], token)
-                if list_licenses != pull_licenses:
-                    print('PULLS:', file=f)
-                    print('master: ', list_licenses, 'head: ', pull_licenses, file=f)
+        process_pull_requests(every_repo, f, list_licenses, token)
 
         print('---------------------------------------', file=f)
     print()
     print('REPOSITORIES TOTAL: {}'.format(len(list_all_repos)))
     print('REPOSITORIES TOTAL: {}'.format(len(list_all_repos)), file=f)
     print()
-    print('REPOSITORIES WITHOUT LICENSE FILE: {}'.format(len(repos_without_license)),':', repos_without_license)
+    print('REPOSITORIES WITHOUT LICENSE FILE: {}'.format(len(repos_without_license)), ':', repos_without_license)
     print('REPOSITORIES WITHOUT LICENSE FILE: {}'.format(len(repos_without_license)), ':', repos_without_license, file=f)
     print()
     print('REPOSITORIES WITHOUT LICENSE: {}'.format(len(list_without_licence)), ':', list_without_licence)
     print('REPOSITORIES WITHOUT LICENSE: {}'.format(len(list_without_licence)), ':', list_without_licence, file=f)
     print()
-
     f.close()
-    compare_files(f.name)
-
-
-def package_json_dependencies(every_repo, f, filename, token):
-    if filename['name'] == 'package.json':
-        dep = dependencies_listing(filename, token)
-        parsed_dependencies = json.loads(dep)
-        for name_dep in parsed_dependencies:
-            if name_dep == 'dependencies' or name_dep == 'devDependencies':
-                list_dependencies = []
-                for name, version in parsed_dependencies[name_dep].items():
-                    d = check_dependencies_license(every_repo, name, version)
-                    list_dependencies.append((name, version, d))
-                if list_dependencies != []:
-                    print(name_dep, ': ', file=f)
-                for s in list_dependencies:
-                    print(s, file=f)
 
 
 def create_file():
-    global f
     datestring = datetime.strftime(datetime.now(), '%Y-%m-%d_%H:%M:%S')
     pattern = 'license_check_' + datestring + '.txt'
-    f = open(pattern, 'w')
+    return open(pattern, 'w')
 
 
-def all_repositories(token):
-    repos_sum_url = json_parsing('https://api.github.com/users/apiaryio', token)
+def all_repositories(token, f):
+    repos_sum_url = json_parsing('https://api.github.com/users/apiaryio', token, f)
     repos_sum = repos_sum_url['public_repos']
     list_all_repos = []
     for number in range(1, repos_sum//30 + 2):
-        all_repos_url = json_parsing('https://api.github.com/users/apiaryio/repos?page= {}'.format(number), token)
+        all_repos_url = json_parsing('https://api.github.com/users/apiaryio/repos?page= {}'.format(number), token, f)
         for repo in all_repos_url:
             list_all_repos.append(repo['name'])
     return list_all_repos
+
 
 def get_token():
     with open('auth_key.txt', encoding='utf-8') as file:
@@ -108,7 +89,7 @@ def get_token():
         return token
 
 
-def json_parsing(url, token):
+def json_parsing(url, token, f):
     content = requests.get(url, auth=HTTPBasicAuth('', token))
     if content.status_code != 200:
         print(content.status_code)
@@ -130,18 +111,20 @@ def base64_decoding(content):
     return text
 
 
-def search_license_readme_packagejson(file, token):
-    parsed_text = json_parsing(file['url'], token)
+def search_license_type_in_readme_packagejson(file, token, f):
+    parsed_text = json_parsing(file['url'], token, f)
     decoded_text = base64_decoding(parsed_text['content'])
     if re.search(r'\blicen[sc]ed?s?', decoded_text, re.I):
-        return license_kind(decoded_text)
+        return license_type(decoded_text)
 
-def search_license_license(file, token):
-    parsed_text = json_parsing(file['url'], token)
+
+def search_license_type_in_license_file(file, token, f):
+    parsed_text = json_parsing(file['url'], token, f)
     decoded_text = base64_decoding(parsed_text['content'])
-    return license_kind(decoded_text)
+    return license_type(decoded_text)
 
-def license_kind(text):
+
+def license_type(text):
     if re.search(r'\bmit\b', text, re.I):
         license = 'MIT'
     elif re.search(r'\bbsd\b', text, re.I):
@@ -153,13 +136,29 @@ def license_kind(text):
     return license
 
 
-def dependencies_listing(file, token):
-    parsed_text = json_parsing(file['url'], token)
+def dependencies_listing(file, token, f):
+    parsed_text = json_parsing(file['url'], token, f)
     decoded_text = base64_decoding(parsed_text['content'])
     return decoded_text
 
 
-def check_dependencies_license(repo, name, version):
+def package_json_dependencies(every_repo, f, filename, token):
+    if filename['name'] == 'package.json':
+        dep = dependencies_listing(filename, token, f)
+        parsed_dependencies = json.loads(dep)
+        for name_dep in parsed_dependencies:
+            if name_dep == 'dependencies' or name_dep == 'devDependencies':
+                list_dependencies = []
+                for name, version in parsed_dependencies[name_dep].items():
+                    d = check_dependencies_license(every_repo, name, version, f)
+                    list_dependencies.append((name, version, d))
+                if list_dependencies != []:
+                    print(name_dep, ': ', file=f)
+                for s in list_dependencies:
+                    print(s, file=f)
+
+
+def check_dependencies_license(repo, name, version, f):
     url = requests.get('https://registry.npmjs.org/' + name + '/' + version)
     if url.status_code != 200:
         print(url.status_code, 'DEPENDENCIES: repo: ', repo, ', name: ', name, ', version: ', version)
@@ -171,31 +170,44 @@ def check_dependencies_license(repo, name, version):
                 if a == x:
                     return b
 
-def head_sha(repo, sha, token):
+
+def pull_requests_search_licence_type(repo, sha, token, f):
     list_tree = []
-    trees_url = json_parsing('https://api.github.com/repos/apiaryio/' + repo + '/git/trees/' + sha, token)
+    trees_url = json_parsing('https://api.github.com/repos/apiaryio/' + repo + '/git/trees/' + sha, token, f)
     for l in trees_url['tree']:
         if re_findall(l['path'], r'\blicen[sc]es?d?'):
-            r = search_license_license(l, token)
+            r = search_license_type_in_license_file(l, token, f)
             list_tree.append((l['path'], r))
         if re_findall(l['path'], r'\breadme') or l['path'] == 'package.json':
-            v = search_license_readme_packagejson(l, token)
+            v = search_license_type_in_readme_packagejson(l, token, f)
             list_tree.append((l['path'], v))
     return list_tree
 
-def compare_files(new_file):
+
+def process_pull_requests(every_repo, f, list_licenses, token):
+    pull_url = json_parsing('https://api.github.com/repos/apiaryio/' + every_repo + '/pulls', token, f)
+    if pull_url != []:
+        for g in pull_url:
+            pull_licenses = pull_requests_search_licence_type(every_repo, g['head']['sha'], token, f)
+            if list_licenses != pull_licenses:
+                print('PULLS:', file=f)
+                print('master: ', list_licenses, 'head: ', pull_licenses, file=f)
+
+
+def compare_files(new_file, old_file):
     datestring = datetime.strftime(datetime.now(), '%Y-%m-%d_%H:%M:%S')
     pattern = 'compare_files' + datestring + '.txt'
     f_02 = open(pattern, 'w')
-    filename = input('Please enter a filename to compare: ')
-    with open(filename, 'r') as f1, open(new_file, 'r') as f2:
+    with open(old_file, 'r') as f1, open(new_file, 'r') as f2:
         diff = difflib.ndiff(f1.readlines(), f2.readlines())
+        f_02.write('File1: {}, File2: {} \n'.format(f1.name, f2.name))
         for i, line in enumerate(diff):
             if line.startswith(' '):
                 continue
             sys.stdout.write('Line: {}, Text: {}'.format(i, line))
             f_02.write('Line: {}, Text: {}'.format(i, line))
     f_02.close()
+
 
 if __name__ == '__main__':
     main()
